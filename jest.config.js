29fbb79bd978b2e725ff72df13b346a02f4db734
Ajin Kabeer <ajiinkabeer@gmail.com>
2020-09-03 12:38:41 +0530

    Add react testing library

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.export = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "\\.(ts|tsx)?$": "babel-jest",
+  },
+  testMatch: ["<rootDir>/src/**/>(*.)test.{ts, tsx}"],
+  moduleFileExtensions: ["ts", "tsx", "json", "node"],
+  testPathIgnorePatterns: ["/node_modules/", "/public/"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+};
---
+++ src/tests/sample.test.ts
@@ -0,0 +1,4 @@
+it("sample test", () => {
+  const a = 10;
+  expect(a).toBe(10);
+});
